38c1e430d806be3c286129920140954e7b84bb33
chehak123 <chehakagrawal01@gmail.com>
2020-11-16 07:25:53 +0530

    modified header,footer and contact page

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
 const cookieParser = require("cookie-parser");
-const connectDB = require("./src/db/mongoose");
+// const connectDB = require("./src/db/mongoose");
 
 const app = express();
 
 // Connect to database
-connectDB();
+// connectDB();
 
 app.get("/", (req, res) => {
   // res.send({ msg: "Api Running" });
@@ -25,7 +25,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/public/views"));
 
 // Define routes
-app.use("/api/users", require("./src/routers/api/userRouter"));
+// app.use("/api/users", require("./src/routers/api/userRouter"));
 
 const PORT = process.env.PORT || 5000;
 
